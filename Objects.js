5ab84ff8cbe7354277456da2be56dc6b1db0e342
sakshigoyal58 <sakshigoyal58@gmail.com>
2021-02-14 05:44:43 +0000

    Objects in js, json.stringify

---
+++ Objects.js
@@ -0,0 +1,8 @@
+const person = {
+  name : "sakshi" ,
+  occupation : "SE",
+  age : 28
+}
+alert(person) // print object 
+alert(JSON.stringify(person)) // will convert object to string with json notation
+alert(Object.values(person)) // will return arraya of values of person
